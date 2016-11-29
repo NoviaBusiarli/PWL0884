34f270a5d6a52af4503c6353e1f0ffd12bb64746
NoviaBusiarli <novia.busiarli@students.amikom.ac.id>
2016-11-29 14:03:47 +0700

    contoh

---
+++ variable.php
@@ -0,0 +1,20 @@
+<?php
+
+	// komentar type 1 baris
+	# komentar type 1 baris juga
+	/* 
+	komentar type 1 baris / lebih
+	*/
+	
+	echo"<h1>Welcome</h1>";
+	$a = "NOVIA BUSIARLI";
+	$abc = 100;
+	$qq = 10.734;
+	
+	echo $a + $abc;
+	echo "\n";
+	echo $qq + $abc;
+	echo "\n";
+	echo "\n";
+	echo $a . $qq . $abc;
+?>
